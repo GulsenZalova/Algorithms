22f278ab5a4aef9af8491ee348270e3c7892d37c
GulsenZalova <zalova0202@gmail.com>
2022-03-26 01:14:05 +0400

    task_5

---
+++ Javascript algotrithms/conditional_exercises/task_5.js
@@ -0,0 +1,9 @@
+// 0-dan 15-ə qədər təkrarlanan JavaScript for loop yazın. Hər iterasiya üçün o, cari nömrənin tək və ya cüt olduğunu yoxlayacaq və ekrana mesaj göstərəcək.
+
+for(let i=0; i<15; i++){
+    if(i%2==0){
+        alert(`${i} ədədi cüt ədəddir.`)
+    }else if(i%2!==0){
+        alert(`${i} ədədi tək ədəddir.`)
+    }
+}
