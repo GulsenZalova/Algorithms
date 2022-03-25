f9dc454a7ff3001f2905cc46d5b101e374b0e983
GulsenZalova <zalova0202@gmail.com>
2022-03-26 03:21:59 +0400

    task_8

--- Javascript algotrithms/conditional_exercises/task_8.js
+++ Javascript algotrithms/conditional_exercises/task_8.js
@@ -1,19 +1,9 @@
 // Üç rəqəmli Armstronq nömrəsi elə tam ədəddir ki, onun rəqəmlərinin kublarının cəmi ədədin özünə bərabər olsun. Məsələn, 371 Armstronq nömrəsidir, çünki 3**3 + 7**3 + 1**3 = 371.
-
-let a=prompt("Birinci ədədi daxil edin:");
-let b=prompt("İkinci ədədi daxil edin:");
-let c=prompt("Üçüncü ədədi daxil edin:");
-
-let akub=Math.pow(a,3);
-let bkub=Math.pow(b,3);
-let ckub=Math.pow(c,3);
-let kublarCemi=akub+bkub+ckub;
-let eded=a*100+b*10+c
-
-if(kublarCemi==eded){
-    alert(kublarCemi);
-    alert("Bu Armstronq nömrəsidir ")
-}else {
-    alert(kublarCemi);
-    alert("Bu Armstronq nömrəsi deyildir")
+let d;
+for(let i=100;i<1000;i++){
+    i=i.toString();
+    d=i.split("")
+    if (Math.pow(d[0],3) + Math.pow(d[1],3)+Math.pow(d[2],3)==i){
+        console.log(i)
+    }
 }
