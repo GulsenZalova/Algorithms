00ab625452a1a17d6a411ef37bf480515dc10b8f
GulsenZalova <zalova0202@gmail.com>
2022-03-26 01:42:27 +0400

    task_6

---
+++ Javascript algotrithms/conditional_exercises/task_6.js
@@ -0,0 +1,15 @@
+// Aşağıdakı tələbələrin orta qiymətlərini hesablayan JavaScript proqramı yazın. Sonra bu ortalama müvafiq qiyməti təyin etmək üçün istifadə olunur.
+
+for(let i=0; i<=100; i++){
+    if(i%3==0){
+        console.log("Fizz");
+        continue;
+    }else if(i%5==0){
+        console.log("Buzz");
+        continue;
+    }else if((i%3==0)&&(i%5==0)){
+        console.log( "FizzBuzz");
+        continue;
+    }
+    console.log(i)
+}
