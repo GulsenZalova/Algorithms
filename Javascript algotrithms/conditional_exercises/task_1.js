2d5c4ea8f4cf6fca2ae935968d5ffc77161cfb15
GulsenZalova <zalova0202@gmail.com>
2022-03-26 00:07:35 +0400

    task_2

--- Javascript algotrithms/conditional_exercises/task_1.js
+++ Javascript algotrithms/conditional_exercises/task_1.js
@@ -1,12 +1,12 @@
 // İki tam ədədi qəbul edən və daha böyük olanı göstərən JavaScript proqramı yazın.
 
-let first=Math.trunc(prompt("İlk ədədi daxil edin: "));
-let second=Math.trunc(prompt("Ikinci ədədi daxil edin: "));
+// let first=Math.trunc(prompt("İlk ədədi daxil edin: "));
+// let second=Math.trunc(prompt("Ikinci ədədi daxil edin: "));
 
-if(first>second){
-    alert(`${first} ədədi daha böyükdür`)
-}else if(second>first){
-    alert(`${second} ədədi daha böyükdür`)
-}else{
-    alert(`Ədədlər bərabərdir.`)
-}
+// if(first>second){
+//     alert(`${first} ədədi daha böyükdür`)
+// }else if(second>first){
+//     alert(`${second} ədədi daha böyükdür`)
+// }else{
+//     alert(`Ədədlər bərabərdir.`)
+// }
