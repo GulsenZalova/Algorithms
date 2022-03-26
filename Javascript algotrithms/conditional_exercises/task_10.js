5f2a17e1af94cfc43c3de89b0f3eb43d4b4df7e1
GulsenZalova <zalova0202@gmail.com>
2022-03-26 04:27:35 +0400

    task_11

---
+++ Javascript algotrithms/conditional_exercises/task_10.js
@@ -0,0 +1,9 @@
+// Ulduzları şam ağacı şəklində qura biləcək Javscript kodu yazın.
+// let sira;
+// for(let i=1; i<=6; i++){
+//     for(let a=1; a<i; a++){
+//     sira=sira+("*");
+//     }
+//     console.log(sira);
+//     sira='';
+// }
--- Javascript algotrithms/conditional_exercises/task_9.js
+++ Javascript algotrithms/conditional_exercises/task_9.js
@@ -1,9 +1,9 @@
 // 3 və 5-in qatlarını 1000-dən aşağı toplamaq üçün JavaScript proqramı yazın.
 
- let sum=0
-for(let i=0;i<1000; i++){
-    if(i%3==0 || i%5==0){
-        sum+=i
-    }
-}
-console.log(sum)
+//  let sum=0
+// for(let i=0;i<1000; i++){
+//     if(i%3==0 || i%5==0){
+//         sum+=i
+//     }
+// }
+// console.log(sum)
