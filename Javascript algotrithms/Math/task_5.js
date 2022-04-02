b178655e4d7fbc59541d1e4520855a4c977737e9
GulsenZalova <zalova0202@gmail.com>
2022-04-03 02:53:00 +0400

    2-dən çox ədədin ƏKOB-nun tapılması

--- Javascript algotrithms/Math/task_5.js
+++ Javascript algotrithms/Math/task_5.js
@@ -1,24 +1,24 @@
 // İki ədədin ƏKOB-nun tapılması
-let a;
-let b;
+// let a;
+// let b;
 
-function ekob(a,b){
-    if(a>b){
-        for(let i=b;i<=a*b; i++){
-            if((i%a==0)&&(i%b==0)){
-                return i
-            }
-        }
-    }else{
-        for(let i=a; i<=a*b; i++){
-            if((i%a==0)&&(i%b==0)){
-                return i
-            }
-        }
-    }
-}
-ekob(35,48)
-console.log(ekob(35,48))
+// function ekob(a,b){
+//     if(a>b){
+//         for(let i=b;i<=a*b; i++){
+//             if((i%a==0)&&(i%b==0)){
+//                 return i
+//             }
+//         }
+//     }else{
+//         for(let i=a; i<=a*b; i++){
+//             if((i%a==0)&&(i%b==0)){
+//                 return i
+//             }
+//         }
+//     }
+// }
+// ekob(35,48)
+// console.log(ekob(35,48))
 
 
 
---
+++ Javascript algotrithms/Math/task_6.js
@@ -0,0 +1,39 @@
+// 2-dən çox ədədin ƏKOB-nun tapılması
+let a;
+let b;
+let c;
+let d;
+
+function ekobTap(a,b,c,d){
+    if(a>c&&b>c&&d>c){
+        for(let i=c; i<=a*b*c*d; i++){
+            if((i%a==0)&&(i%b==0)&&(i%c==0)&&(i%d==0)){
+                return i
+            }
+        }
+    }
+    else if(a>d&&b>d&&c>d){
+        for(let i=d; i<=a*b*c*d; i++){
+            if((i%a==0)&&(i%b==0)&&(i%c==0)&&(i%d==0)){
+                return i
+            }
+        }
+    }
+    else if(a>b&&c>b&&d>b){
+        for(let i=b; i<=a*b*c*d; i++){
+            if((i%a==0)&&(i%b==0)&&(i%c==0)&&(i%d==0)){
+                return i
+            }
+        }
+    }
+    else if(b>a&&c>a&&d>a){
+        for(let i=a; i<=a*b*c*d; i++){
+            if((i%a==0)&&(i%b==0)&&(i%c==0)&&(i%d==0)){
+                return i
+            }
+        }
+    }
+
+}
+ekobTap(5,10,15,25)
+console.log(ekobTap(5,10,15,25))
