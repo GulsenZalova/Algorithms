56b0e08fdc610d85f15c06bc0dfd0601668645ca
GulsenZalova <zalova0202@gmail.com>
2022-04-11 00:56:06 +0400

    array task_4

---
+++ Javascript algotrithms/Array/array_4.js
@@ -0,0 +1,9 @@
+// Massivin son elementini əldə etmək üçün JavaScript funksiyasını yazın.
+function showElement(arr,n){
+    if(n==null){
+        return arr.slice(-1)
+    }else if(n>0){
+        return arr.slice(-n)
+    }
+}
+console.log(showElement([7, 9, 0, -2,],6))
