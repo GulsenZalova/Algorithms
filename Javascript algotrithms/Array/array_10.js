b7e64c0e945610efd79611f7e1bb3b790e4a7487
GulsenZalova <zalova0202@gmail.com>
2022-04-12 03:02:40 +0400

    array task_10

---
+++ Javascript algotrithms/Array/array_10.js
@@ -0,0 +1,8 @@
+// Array daxilindəki elementlərin kavadratlar cəmini qaytaran proqram yazın.
+// let arr=[1,2,3,4,5,6]
+// let sum=0;
+// for(let index of arr){
+//     let square=Math.pow(index,2);
+//     sum+=square
+// }
+// console.log(sum)
--- Javascript algotrithms/Array/array_9.js
+++ Javascript algotrithms/Array/array_9.js
@@ -1,21 +1,22 @@
-let sentence='The Quick Brown Fox';
-let Upper='ABCDEFGHIJKLMNOPQRSTUVWXYZ';
-let Lower='abcdefghijklmnopqrstuvwxyz';
-let arr=[]
+// Cümlədəki böyük hərfləri kiçik, kiçik hərfləri böyük yazan proqram yazın.
+// let sentence='The Quick Brown Fox';
+// let Upper='ABCDEFGHIJKLMNOPQRSTUVWXYZ';
+// let Lower='abcdefghijklmnopqrstuvwxyz';
+// let arr=[]
 
 
-for(let i=0; i<sentence.length; i++){
-    if(Upper.includes(sentence[i])){
-        arr.push(sentence[i].toLowerCase())
-    }
-    else if(Lower.includes(sentence[i])){
-        arr.push(sentence[i].toUpperCase())
-    }
-    else{
-        arr.push(sentence[i])
-    }
-} 
-console.log(arr.join(''))
+// for(let i=0; i<sentence.length; i++){
+//     if(Upper.includes(sentence[i])){
+//         arr.push(sentence[i].toLowerCase())
+//     }
+//     else if(Lower.includes(sentence[i])){
+//         arr.push(sentence[i].toUpperCase())
+//     }
+//     else{
+//         arr.push(sentence[i])
+//     }
+// } 
+// console.log(arr.join(''))
 
 
 
