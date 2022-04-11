94809e16e40752bfc8e39063074a8ffbf367b930
GulsenZalova <zalova0202@gmail.com>
2022-04-12 03:18:24 +0400

    array task_11

---
+++ Javascript algotrithms/Array/array_11.js
@@ -0,0 +1,13 @@
+// Array daxilindəki tam ədədlərin cəm və hasilini tapın
+let arr = [1, 2.3, 5, 6.7, 8]
+let sum=0
+let multi=1
+for(let index of arr ){
+
+    if(index-Math.floor(index)==0){
+        sum+=index
+        multi*=index
+    }
+}
+console.log(sum)
+console.log(multi)
