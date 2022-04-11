2f219a88620de2fc014c5d79a879a326c4bb66e8
GulsenZalova <zalova0202@gmail.com>
2022-04-12 03:40:58 +0400

    array task_12

---
+++ Javascript algotrithms/Array/array_12.js
@@ -0,0 +1,7 @@
+// Buttona clicklədikdə array elementi boş imputa daxil olsun
+function myfunction(){
+    let arr=[1,2,3,4,5]
+    for(let i=0; i<arr.length; i++){
+    document.getElementById("demo").innerHTML=arr[i]
+    }
+}
